c59cf03b20e377a96d1ec85556be453521bbed17
Jeffrey Zelaya <jeffreyz.dev@gmail.com>
2020-03-30 15:05:53 -0600

    got it working without all the packages

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -33,51 +33,48 @@
 
 
 
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import $ from "jquery";
-// import "turn.js";
+import React from "react";
+import $ from "jquery";
+import "turn.js";
 
 
-// import Turn from "./turn";
+import Turn from "./turn";
 
-// const options = {
-//   width: 800,
-//   height: 600,
-//   autoCenter: true,
-//   display: "double",
-//   acceleration: true,
-//   elevation: 50,
-//   gradients: !$.isTouch,
-//   when: {
-//     turned: function(e, page) {
-//       console.log("Current view: ", $(this).turn("view"));
-//     }
-//   }
-// };
+const options = {
+  width: 800,
+  height: 600,
+  autoCenter: true,
+  display: "double",
+  acceleration: true,
+  elevation: 50,
+  gradients: !$.isTouch,
+  when: {
+    turned: function(e, page) {
+      console.log("Current view: ", $(this).turn("view"));
+    }
+  }
+};
 
-// const pages = [
-//   "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/01.jpg",
-//   "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/02.jpg",
-//   "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/03.jpg",
-//   "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/04.jpg",
-//   "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/05.jpg",
-//   "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/06.jpg"
-// ];
+const pages = [
+  "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/01.jpg",
+  "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/02.jpg",
+  "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/03.jpg",
+  "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/04.jpg",
+  "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/05.jpg",
+  "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/06.jpg"
+];
 
-// const Home = () => {
-//   return (
-//     <Turn options={options} className="magazine">
-//       {pages.map((page, index) => (
-//         <div key={index} className="page">
-//           <img src={page} alt="" />
-//         </div>
-//       ))}
-//     </Turn>
-//   );
-// };
+const Home = () => {
+  return (
+    <Turn options={options} className="magazine">
+      {pages.map((page, index) => (
+        <div key={index} className="page">
+          <img src={page} alt="" />
+        </div>
+      ))}
+    </Turn>
+  );
+};
 
-// export default Home
+export default Home
 
-// // const rootElement = document.getElementById("root");
-// // ReactDOM.render(<Home />, rootElement);
